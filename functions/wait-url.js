de95e45743de1f4d242c3fdf33ff2d492b8ac900
Katsumi Takahashi <katsumi@gij.com>
2022-12-14 10:54:46 +0900

    content-typeを追加

--- functions/wait-url.js
+++ functions/wait-url.js
@@ -2,10 +2,16 @@ exports.handler = async function (context, event, callback) {
   console.log(`🐞 wait-url called. ${event.request.headers.host}`);
   const DOMAIN_NAME = event.request.headers.host;
   try {
+    const response = new Twilio.Response();
+    response.appendHeader('Access-Control-Allow-Origin', '*');
+    response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS, POST, GET');
+    response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
     const VoiceResponse = require('twilio').twiml.VoiceResponse;
-    const response = new VoiceResponse();
-    response.play(`https://${DOMAIN_NAME}/speech.mp3`);
-    callback(null, response.toString());
+    const twiml = new VoiceResponse();
+    twiml.play(`https://${DOMAIN_NAME}/speech.mp3`);
+    response.appendHeader('Content-Type', 'application/xml');
+    response.setBody(twiml);
+    callback(null, twiml);
   } catch (err) {
     console.error(`👺 ERROR: ${err.message ? err.message : err}`);
     callback(err);
